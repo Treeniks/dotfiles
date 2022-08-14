73959e1cb59ae463b4f818febfde5f3eff744921
Thomas Lindae <thomas.lindae@in.tum.de>
2022-08-15 01:28:09 +0200

    alacritty: added setup

---
+++ alacritty/link.jl
@@ -0,0 +1,20 @@
+link_target_path = "$(@__DIR__)/alacritty.yml"
+if Sys.iswindows()
+    config_path = "$(ENV["APPDATA"])/alacritty/alacritty.yml"
+else
+    println("OS not yet supported")
+    exit()
+end
+
+if isfile(config_path)
+    print("Config already exists, overwrite? [y/N]: ")
+    answer = readline()
+    if answer == "y" || answer == "Y"
+        rm(config_path)
+    else
+        exit()
+    end
+end
+
+symlink(link_target_path, config_path)
+println("Symlink created")
