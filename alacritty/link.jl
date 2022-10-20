b731d2450d5655c78e127be713f29602a25f373c
Thomas Lindae <thomas.lindae@in.tum.de>
2022-10-20 14:41:38 +0200

    alacritty: added apple config path

--- alacritty/link.jl
+++ alacritty/link.jl
@@ -1,6 +1,8 @@
 link_target_path = "$(@__DIR__)/alacritty.yml"
 if Sys.iswindows()
     config_path = "$(ENV["APPDATA"])/alacritty/alacritty.yml"
+elseif Sys.isapple()
+    config_path = "$(ENV["HOME"])/.config/alacritty/alacritty.yml"
 else
     println("OS not yet supported")
     exit()
