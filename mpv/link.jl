19dbad0ab399db6c37416e4324aa01c817c0fac0
Thomas Lindae <thomas.lindae@in.tum.de>
2022-11-01 13:41:45 +0100

    mpv: added config

---
+++ mpv/link.jl
@@ -0,0 +1,20 @@
+link_target_path = "$(@__DIR__)/config"
+if Sys.iswindows()
+    config_path = "$(ENV["USERPROFILE"])/scoop/persist/mpv/portable_config"
+else
+    println("OS not yet supported")
+    exit()
+end
+
+if ispath(config_path)
+    print("Config already exists, overwrite? [y/N]: ")
+    answer = readline()
+    if answer == "y" || answer == "Y"
+        rm(config_path, recursive=true)
+    else
+        exit()
+    end
+end
+
+symlink(link_target_path, config_path, dir_target=true)
+println("Symlink created")
