0c8d84d65774c27e7594dd27992d8ac0e50b9335
Thomas Lindae <thomas.lindae@in.tum.de>
2023-10-12 03:24:33 +0200

    nvim removed scope highlighting

--- nvim/nvim/after/plugin/misc.lua
+++ nvim/nvim/after/plugin/misc.lua
@@ -5,7 +5,7 @@ require('telescope').load_extension('project')
 require('toggleterm').setup{}
 
 -- indentation guides
-require('ibl').setup{}
+require('ibl').setup{ scope = { enabled = false } }
 
 -- status line at the bottom
 require('lualine').setup{}
