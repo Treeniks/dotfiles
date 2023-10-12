7aecfbc1fad38de7f01ffdb2585977d6b28f8d4e
Thomas Lindae <thomas@lindae.de>
2023-10-12 22:13:50 +0200

    nvim: added latex to treesitter

--- nvim/nvim/after/plugin/treesitter.lua
+++ nvim/nvim/after/plugin/treesitter.lua
@@ -1,7 +1,7 @@
 require('tree-sitter-just').setup({})
 
 require('nvim-treesitter.configs').setup {
-    ensure_installed = { 'c', 'lua', 'rust', 'markdown', 'markdown_inline', 'javascript', 'typescript', 'zig', 'haskell' },
+    ensure_installed = { 'c', 'lua', 'rust', 'markdown', 'markdown_inline', 'javascript', 'typescript', 'zig', 'haskell', 'latex' },
 
     sync_install = false,
 
