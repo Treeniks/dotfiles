81c77d1c706749c7446c38902358673b10226cb8
Thomas Lindae <thomas@lindae.de>
2023-12-15 06:40:44 +0100

    nvim: switched to nvim-surround

--- nvim/nvim/lua/plugins.lua
+++ nvim/nvim/lua/plugins.lua
@@ -41,7 +41,14 @@ require('lazy').setup({
 
     -- Mastering the vim language plugins
     'tpope/vim-repeat',
-    'tpope/vim-surround',
+    {
+        "kylechui/nvim-surround",
+        version = "*", -- use for stability
+        event = "VeryLazy",
+        config = function()
+            require('nvim-surround').setup({})
+        end,
+    },
     'christoomey/vim-system-copy',
     'vim-scripts/ReplaceWithRegister',
     'mg979/vim-visual-multi',
