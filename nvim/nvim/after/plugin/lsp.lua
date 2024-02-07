e61242842d7dd6b6b911c95f45fbae72756c892b
Thomas Lindae <thomas.lindae@in.tum.de>
2024-02-07 09:06:54 +0100

    nvim: fixed snippets breaking Tab

--- nvim/nvim/after/plugin/lsp.lua
+++ nvim/nvim/after/plugin/lsp.lua
@@ -21,23 +21,27 @@ cmp.setup({
         ['<Tab>'] = cmp.mapping(function(fallback)
             if cmp.visible() then
                 cmp.confirm({ select = true })
-            -- elseif luasnip.expandable() then
-            --     luasnip.expand()
-            -- elseif luasnip.expand_or_jumpable() then
-            --     luasnip.expand_or_jump()
-            elseif luasnip.jumpable(1) then
-                luasnip.jump(1)
             else
                 fallback()
             end
         end, { 'i', 's' }),
-        ['<S-Tab>'] = cmp.mapping(function(fallback)
+        -- jumping between snippet positions is on a different keybind
+        -- because otherwise it would jump to previous snippet positions
+        -- when trying to just write a tab somewhere else
+        ['<C-l>'] = cmp.mapping(function(fallback)
+            if luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+            else
+                fallback()
+            end
+        end, { 'i', 's' }),
+        ['<C-h>'] = cmp.mapping(function(fallback)
             if luasnip.jumpable(-1) then
                 luasnip.jump(-1)
             else
                 fallback()
             end
-        end, { 'i', 's' })
+        end, { 'i', 's' }),
     },
     sources = cmp.config.sources({
         { name = 'luasnip' },
