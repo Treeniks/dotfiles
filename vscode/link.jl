8a1b8c89b2db4c0f69e025e0fe75531140a223fd
Thomas Lindae <thomas@lindae.de>
2024-04-24 18:32:13 +0200

    vscode: added linux path

--- vscode/link.jl
+++ vscode/link.jl
@@ -1,14 +1,15 @@
 link_target_path = "$(@__DIR__)/settings.json"
 if Sys.iswindows()
     config_dir = "$(ENV["USERPROFILE"])/scoop/persist/vscode/data/user-data/User"
-    config_path = "$config_dir/settings.json"
 elseif Sys.isapple()
     config_dir = "$(ENV["HOME"])/Library/Application Support/Code/User"
-    config_path = "$config_dir/settings.json"
+elseif Sys.islinux()
+    config_dir = "$(ENV["HOME"])/.config/Code/User"
 else
     println("OS not yet supported")
     exit()
 end
+config_path = "$config_dir/settings.json"
 
 if isfile(config_path)
     print("Config already exists, overwrite? [y/N]: ")
