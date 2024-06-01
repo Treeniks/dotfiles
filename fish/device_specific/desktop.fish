cb8851d4e590184888df1d7e976784eb3f132960
Thomas Lindae <thomas.lindae@in.tum.de>
2024-06-01 03:43:37 +0200

    fish: desktop path stuff

--- fish/device_specific/desktop.fish
+++ fish/device_specific/desktop.fish
@@ -1,5 +1,7 @@
 set -gx XDG_CONFIG_HOME "$HOME/.config"
 
+fish_add_path -g "$HOME/Downloads/node-v20.13.1-linux-x64/bin/"
+
 # Path
 eval (opam env)
 fish_add_path -g "$HOME/.local/bin/"
@@ -7,10 +9,11 @@ fish_add_path -g "$HOME/.cargo/bin/"
 fish_add_path -g "$HOME/.nimble/bin/"
 fish_add_path -g "$HOME/.dotnet/tools/"
 
-# fish_add_path -g "$HOME/Documents/isabelle/isabelle-emacs/bin/"
-fish_add_path -g "$HOME/Documents/isabelle/isabelle-git/bin/"
-fish_add_path -g "$HOME/Documents/odin/Odin/"
 fish_add_path -g "$HOME/.local/share/coursier/bin/"
+
+fish_add_path -g "$HOME/Documents/isabelle/isabelle-language-server/bin/"
+fish_add_path -g "$HOME/Documents/odin/Odin/"
+fish_add_path -g "$HOME/Documents/git-remote-hg/"
 
 function animemode
     switch $argv[1]
@@ -25,7 +28,7 @@ function animemode
                 echo "Gammastep not running"
             end
 
-            hyprctl --batch "keyword monitor HDMI-A-2,3840x2160@60,0x0,2,bitdepth,10; keyword monitor DP-2,disable; keyword monitor HDMI-A-1,disable; keyword workspace name:DP-2_1,persistent:false; keyword workspace name:DP-2_2,persistent:false; keyword workspace name:DP-2_3,persistent:false; keyword workspace name:HDMI-A-1_1,persistent:false; keyword workspace name:HDMI-A-1_2,persistent:false; keyword workspace name:HDMI-A-1_3,persistent:false;"
+            hyprctl --batch "keyword monitor HDMI-A-2,3840x2160@120,0x0,2,bitdepth,10; keyword monitor DP-2,disable; keyword monitor HDMI-A-1,disable; keyword workspace name:DP-2_1,persistent:false; keyword workspace name:DP-2_2,persistent:false; keyword workspace name:DP-2_3,persistent:false; keyword workspace name:HDMI-A-1_1,persistent:false; keyword workspace name:HDMI-A-1_2,persistent:false; keyword workspace name:HDMI-A-1_3,persistent:false;"
         case off
             # `2>&1` redirects stderr to stdout
             # `> /dev/null` to suppress output
