7ae3a35322cc82684f7d4cca5330426ec6ad7c0b
Thomas Lindae <thomas.lindae@in.tum.de>
2024-06-01 03:43:57 +0200

    nvim: lsp rounded borders

--- nvim/nvim/after/plugin/lsp.lua
+++ nvim/nvim/after/plugin/lsp.lua
@@ -2,14 +2,19 @@ local lspconfig = require('lspconfig')
 local luasnip = require('luasnip')
 
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-    vim.lsp.diagnostic.on_publish_diagnostics, {
-        virtual_text = false
-    }
+    vim.lsp.diagnostic.on_publish_diagnostics, { virtual_text = false }
 )
 
-vim.diagnostic.config {
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+    vim.lsp.handlers.hover, { border = 'rounded' }
+)
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
+    vim.lsp.handlers.signature_help, { border = 'rounded' }
+)
+
+vim.diagnostic.config({
     float = { border = "rounded" },
-}
+})
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
