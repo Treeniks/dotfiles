4b563aa0f3a12deed604fdda5622e7f438d17aa7
Thomas Lindae <thomas@lindae.de>
2024-07-07 12:37:37 +0200

    fish asahi: opam and path adjustments

--- fish/device_specific/asahi.fish
+++ fish/device_specific/asahi.fish
@@ -1,8 +1,12 @@
+eval (opam env)
+
 fish_add_path -g "$HOME/Documents/julia-1.9.4/bin/"
 fish_add_path -g "$HOME/Documents/zig/zig-linux-aarch64-0.11.0/"
 
 fish_add_path -g "$HOME/.cargo/bin/"
-fish_add_path -g "$HOME/Documents/isabelle/isabelle-emacs/bin/"
+fish_add_path -g "$HOME/Documents/isabelle/isabelle-language-server/bin/"
 
 fish_add_path -g "$HOME/Documents/sublime_text/"
 fish_add_path -g "$HOME/Documents/sublime_merge/"
+
+fish_add_path -g "$HOME/Documents/git-remote-hg/"
