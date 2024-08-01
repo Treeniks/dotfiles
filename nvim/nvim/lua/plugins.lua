bbc3bfe76db8ab6685078cffc4d08159527a4a43
Thomas Lindae <thomas.lindae@in.tum.de>
2024-08-01 22:38:21 +0200

    nvim: improved indent-blankline setup to support tabs and scopes

--- nvim/nvim/lua/plugins.lua
+++ nvim/nvim/lua/plugins.lua
@@ -135,8 +135,16 @@ require('lazy').setup({
         'lukas-reineke/indent-blankline.nvim',
         main = 'ibl',
         opts = {
-            indent = { highlight = 'Comment', char = '▏' },
-            scope = { enabled = false },
+            indent = {
+                highlight = 'Comment',
+                char = '▏',
+                tab_char = '▏'
+            },
+            scope = {
+                enabled = true,
+                show_start = false,
+                show_end = false,
+            },
         },
     },
 
