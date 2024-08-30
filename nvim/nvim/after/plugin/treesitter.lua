19bcd6902ed1f894db48ad0f83c7ebb9d5498fc5
Thomas Lindae <thomas.lindae@in.tum.de>
2024-08-30 15:31:37 +0200

    nvim: added c3 syntax highlighting

--- nvim/nvim/after/plugin/treesitter.lua
+++ nvim/nvim/after/plugin/treesitter.lua
@@ -52,6 +52,23 @@ require('nvim-treesitter.configs').setup({
     },
 })
 
+vim.filetype.add({
+    extension = {
+        c3 = "c3",
+        c3i = "c3",
+        c3t = "c3",
+    },
+})
+
+local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+parser_config.c3 = {
+    install_info = {
+        url = "https://github.com/c3lang/tree-sitter-c3",
+        files = {"src/parser.c", "src/scanner.c"},
+        branch = "main",
+    },
+}
+
 -- require('tree-sitter-isar')
 
 require('treesitter-context').setup({
