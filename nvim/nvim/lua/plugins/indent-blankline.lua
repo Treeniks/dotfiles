31b9e4d29e0f72648c06adb117430cfd83b6aa3c
Thomas Lindae <thomas@lindae.de>
2024-11-10 01:36:06 +0100

    nvim: indent-blankline

---
+++ nvim/nvim/lua/plugins/indent-blankline.lua
@@ -0,0 +1,15 @@
+return {
+    'lukas-reineke/indent-blankline.nvim',
+    main = 'ibl',
+    opts = {
+        indent = {
+            char = '▏',
+            tab_char = '▏'
+        },
+        scope = {
+            enabled = true,
+            show_start = false,
+            show_end = false,
+        },
+    },
+}
