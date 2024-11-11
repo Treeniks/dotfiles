b934443bbf4af06243396fd2a06ab25cf0d68e5b
Thomas Lindae <thomas.lindae@in.tum.de>
2024-11-11 08:29:56 +0100

    fish macos: texlive + homebrew no auto update

--- fish/device_specific/macos.fish
+++ fish/device_specific/macos.fish
@@ -13,10 +13,15 @@ fish_add_path -g "$(brew --prefix)/bin" "$(brew --prefix)/sbin"
 fish_add_path -g "$(brew --prefix python)/libexec/bin"
 
 # Opam
-# source /Users/thomas/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
 eval (opam env)
 # MacOS comes with bison preinstalled, but I want to use brew's bison
 fish_add_path -g "/opt/homebrew/opt/bison/bin"
+
+# if homebrew's gcc or clang are needed
+# fish_add_path -g "/opt/homebrew/Cellar/llvm/19.1.1/bin"
+# alias gcc gcc-14
+# alias c++ c++-14
+
 fish_add_path -g "$HOME/Library/Application Support/JetBrains/Toolbox/scripts"
 fish_add_path -g "$HOME/.cargo/bin"
 fish_add_path -g "$HOME/.nimble/bin"
@@ -24,7 +29,7 @@ fish_add_path -g "$HOME/.ghcup/bin"
 fish_add_path -g "$HOME/.dotnet/tools"
 
 fish_add_path -g "$HOME/Documents/dotfiles/bin/"
-fish_add_path -g "$HOME/Documents/isabelle/isabelle-language-server/bin/"
+fish_add_path -g "$HOME/Documents/isabelle/isabelle/bin/"
 fish_add_path -g "$HOME/Documents/git-remote-hg/"
 
 fish_add_path -g "$HOME/Documents/zig/zig-macos-aarch64-0.12.0-dev.1769+bf5ab5451"
@@ -33,12 +38,19 @@ fish_add_path -g "$HOME/Documents/vale/Vale-Mac-0.2.0.28"
 
 fish_add_path -g "$HOME/Library/Application Support/Coursier/bin"
 
+# texlive
+set -gx MANPATH $MANPATH "/usr/local/texlive/2024/texmf-dist/doc/man"
+set -gx INFOPATH $INFOPATH "/usr/local/texlive/2024/texmf-dist/doc/info"
+fish_add_path -g "/usr/local/texlive/2024/bin/universal-darwin"
+
 # Launch GPG Agent
 gpgconf --launch gpg-agent
 
 # Set GPG Agent as SSH Agent
 set -gx SSH_AGENT_PID ""
 set -gx SSH_AUTH_SOCK "$(gpgconf --list-dirs agent-ssh-socket)"
+
+set -gx HOMEBREW_NO_AUTO_UPDATE 1
 
 # start window manager
 # (not needed, as it's started as a MacOS Login Item)
