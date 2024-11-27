126a8b09e7996f0d1b2c5f5361a98a1c308e7566
Thomas Lindae <thomas@lindae.de>
2024-11-27 23:40:22 +0100

    fish asahi

--- fish/device_specific/asahi.fish
+++ fish/device_specific/asahi.fish
@@ -1,7 +1,9 @@
+# asahi uses nix because Fedora's repos are kinda bare
+source "$HOME/.nix-profile/etc/profile.d/nix.fish"
+
 # eval (opam env)
 
 fish_add_path -g "$HOME/.cargo/bin/"
-# fish_add_path -g "$HOME/Documents/isabelle/isabelle-language-server/bin/"
 
 fish_add_path -g "/opt/sublime_text/"
 fish_add_path -g "/opt/sublime_merge/"
