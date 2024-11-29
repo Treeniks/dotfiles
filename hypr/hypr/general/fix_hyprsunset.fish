45314c5618f16b7318b6c4d2083260a13768c80f
Thomas Lindae <thomas@lindae.de>
2024-11-29 13:13:37 +0100

    hypr: made screen temperature an envvar and fixed fix script breaking some screens

--- hypr/hypr/general/fix_hyprsunset.fish
+++ hypr/hypr/general/fix_hyprsunset.fish
@@ -6,11 +6,12 @@ set pid (pgrep hyprsunset)
 if test -n $pid
     kill $pid
     echo "hyprsunset stopped"
+    sleep 1
 else
     echo "hyprsunset not running"
 end
 
 # `2>&1` redirects stderr to stdout
 # `> /dev/null` to suppress output
-hyprsunset -t 5500 > /dev/null 2>&1 &
+hyprsunset -t $SCREEN_TEMP > /dev/null 2>&1 &
 disown $last_pid
