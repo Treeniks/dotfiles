f744e282f6471a1fe5080defed617c53615a177c
Thomas Lindae <thomas@lindae.de>
2025-10-04 23:15:56 +0200

    added and switched to niri (trial run)

---
+++ niri/link.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from sys import path
+from os.path import realpath, dirname
+path.append(dirname(dirname(realpath(__file__))))
+from utils import *
+
+check_os(OS.LINUX)
+
+target = join(target_dir(), "niri")
+link = join(DOTCONFIG, "niri")
+
+make_symlink(target, link)
