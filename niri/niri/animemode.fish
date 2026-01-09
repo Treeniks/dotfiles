411aae649c7303186bfefd4c93d0c388c7ad8e74
Thomas Lindae <thomas@lindae.de>
2026-01-09 18:23:30 +0100

    niri: switched to AMG GPU

--- niri/niri/animemode.fish
+++ niri/niri/animemode.fish
@@ -3,13 +3,13 @@
 switch $argv[1]
     case on
         killall gammastep
-        niri msg output HDMI-A-2 scale 2
-        niri msg output DP-3 off
+        niri msg output HDMI-A-1 scale 2
+        niri msg output DP-1 off
         niri msg output DP-2 off
     case off
         disown $last_pid
-        niri msg output HDMI-A-2 scale 1.5
-        niri msg output DP-3 on
+        niri msg output HDMI-A-1 scale 1.5
+        niri msg output DP-1 on
         niri msg output DP-2 on
         sleep 2
         gammastep -O 4500 &
